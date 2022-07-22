4060f3fd94e290c74a2ac31796f298533077035b
Heecheoljjang <kkll135@naver.com>
2022-07-23 01:53:01 +0900

    이름 변경 화면 UI구현

---
+++ Tamagotchi/Name/NameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NameViewController.swift
+//  Tamagotchi
+//
+//  Created by HeecheolYoon on 2022/07/23.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
